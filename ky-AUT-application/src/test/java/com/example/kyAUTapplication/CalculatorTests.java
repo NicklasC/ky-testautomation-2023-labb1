e2cb6acb0f147d484e224feea960ba24e49b7b9d
Nicklas Carlsson <nicklas.carlsson@gmail.com>
2023-02-28 14:50:47 +0100

    Add Calculator and Validator. Also add corresponding empty test files.

---
+++ ky-AUT-application/src/test/java/com/example/kyAUTapplication/CalculatorTests.java
@@ -0,0 +1,13 @@
+package com.example.kyAUTapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ky-AUT-application/src/test/java/com/example/kyAUTapplication/ValidatorTests.java
@@ -0,0 +1,13 @@
+package com.example.kyAUTapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatorTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
