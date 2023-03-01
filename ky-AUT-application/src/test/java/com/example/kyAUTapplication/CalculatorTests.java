ff940a82dbcbac2ae42e0d33f700028f4c581bfe
Nicklas Carlsson <nicklas.carlsson@gmail.com>
2023-03-01 09:53:49 +0100

    Add unit test cases covering Calculator and Validator classes

--- ky-AUT-application/src/test/java/com/example/kyAUTapplication/CalculatorTests.java
+++ ky-AUT-application/src/test/java/com/example/kyAUTapplication/CalculatorTests.java
@@ -1,13 +1,65 @@
 package com.example.kyAUTapplication;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CalculatorTests {
 
-	@Test
-	void contextLoads() {
-	}
 
+    private Calculator calculator = new Calculator();
+
+    @Test
+    void testAdd() {
+        Assertions.assertEquals(8, calculator.add(3, 5));
+    }
+
+    @Test
+    void testSubtract() {
+        Assertions.assertEquals(7, calculator.subtract(10, 3));
+    }
+
+    @Test
+    void testMultiply() {
+        Assertions.assertEquals(15, calculator.multiply(3, 5));
+    }
+
+    @Test
+    void testDivide() {
+        Assertions.assertEquals(5, calculator.divide(25, 5));
+    }
+
+    @Test
+    void testDivideWithDecimals() {
+        Assertions.assertEquals(3.5714285714285716, calculator.divide(25, 7));
+    }
+
+    @Test
+    void testSquareRoot() {
+        Assertions.assertEquals(2, calculator.squareRootOf(4));
+    }
+
+    @Test
+    void testGetAreaAreaIsCorrect() {
+        Assertions.assertEquals(78.54, calculator.getArea(5));
+    }
+
+    @Test
+    void testGetAreaReturnNaNWithNegativeInput() {
+        Assertions.assertEquals(Double.NaN, calculator.getArea(-3));
+    }
+
+    @Test
+    void testCircumference() {
+        Assertions.assertEquals(18.85, calculator.getCircumference(3));
+    }
+
+    @Test
+    void testCircumferenceReturnNaNWithNegativeInput() {
+        Assertions.assertEquals(Double.NaN, calculator.getCircumference(-1));
+    }
 }
+
+
+
--- ky-AUT-application/src/test/java/com/example/kyAUTapplication/ValidatorTests.java
+++ ky-AUT-application/src/test/java/com/example/kyAUTapplication/ValidatorTests.java
@@ -1,13 +1,51 @@
 package com.example.kyAUTapplication;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ValidatorTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void phoneNumberStartsWith0IsOK() {
+        Assertions.assertEquals(true, Validator.isPhoneNumberValid("040444555"));
+    }
 
+    @Test
+    void phoneNumberStartsWith1IsNotOK() {
+        Assertions.assertEquals(false, Validator.isPhoneNumberValid("140444555"));
+    }
+
+    @Test
+    void phoneNumberLengthLessThenSixIsNotOk() {
+        //Start phonenumber with 0 as any phonenumber, no matter the length, is invalid if it does not start with 0.
+        Assertions.assertEquals(false, Validator.isPhoneNumberValid("03355"));
+    }
+
+    @Test
+    void phoneNumberLengthSixIsOK() {
+        Assertions.assertEquals(true, Validator.isPhoneNumberValid("0333555"));
+    }
+
+    @Test
+    void phoneNumberLength10IsOK() {
+        Assertions.assertEquals(true, Validator.isPhoneNumberValid("0333555777"));
+    }
+
+    @Test
+    void phoneNumberLength11IsNotOK() {
+        Assertions.assertEquals(false, Validator.isPhoneNumberValid("03335557778"));
+    }
+
+    @Test
+    void phoneNumberWithDigitsIsOK() {
+        // This test is for completeness and is really tested elsewhere. true / false test together with phoneNumberWithNonDigitsIsNotOK
+        Assertions.assertEquals(true, Validator.isPhoneNumberValid("0333555777"));
+    }
+
+    @Test
+    void phoneNumberWithNonDigitsIsNotOK() {
+        Assertions.assertEquals(false, Validator.isPhoneNumberValid("033355F777"));
+    }
 }
